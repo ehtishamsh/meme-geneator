1e03bc8a5537f50ddbe19e663e63a2d3e2d687bf
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-11-16 03:00:42 +0500

    get img

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,6 @@ import Header from "./components/Header";
 import Meme from "./components/Meme";
 
 function App() {
-  const thingsArray = ["Thing 1", "Thing 2"];
-
-  /**
-   * Challenge: Map over the thingsArray to generate
-   * a <p> element for each item and render them on the page
-   * below the button
-   */
-  const data = thingsArray((item) => {
-    console.log(item);
-  });
   return (
     <>
       <Header />
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import DataObj from "./MemeData.js";
 
 function Meme() {
+  const [url, setUrl] = useState("");
+
+  function handleClick(e) {
+    const dataMemes = DataObj.data.memes;
+    const getUrl =
+      dataMemes[Math.floor(Math.random() * dataMemes.length + 1)].url;
+    setUrl(getUrl);
+    e.preventDefault();
+  }
   return (
     <form>
       <div className="input_div">
-        <input type="text" />
-        <input type="text" />
+        <input type="text" placeholder="Top Text" />
+        <input type="text" placeholder="Bottom Text" />
       </div>
-      <button>Get a new meme image 🖼️</button>
+      <button onClick={handleClick}>Get a new meme image 🖼️</button>
+      <img src={url} alt="" />
     </form>
   );
 }
