1349fe2ed1b91da51f94cc012a6a4cccd758239e
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-11-18 04:03:45 +0500

    added top and bottom text based on state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -13,18 +13,43 @@ function Meme() {
     const data = allImagesData.data.memes;
     const url = data[Math.floor(Math.random() * data.length + 1)].url;
     setMeme((prev) => {
-      return { ...prev, randomImg: url };
+      return { ...prev, randomImg: url, topText: "", bottomText: "" };
     });
     e.preventDefault();
+  }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   }
   return (
     <form>
       <div className="input_div">
-        <input type="text" placeholder="Top Text" />
-        <input type="text" placeholder="Bottom Text" />
+        <input
+          type="text"
+          placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
       </div>
       <button onClick={handleClick}>Get a new meme image 🖼️</button>
-      <img src={meme.randomImg} alt="" className="meme--image" />
+      <div className="imgCon">
+        <img src={meme.randomImg} alt="" className="meme--image" />
+        <div className="topText memeText">{meme.topText}</div>
+        <div className="bottomText memeText">{meme.bottomText}</div>
+      </div>
     </form>
   );
 }
